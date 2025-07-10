dfd1a65516005b373758ad49a15cd79719a665e6
dimachad <dinisma@gmail.com>
2025-07-10 10:18:27 +0200

    Set up python and django lsp, linting and autoformat

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -32,7 +32,9 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black', stop_after_first = true },
+        python = { 'ruff' },
+        htmldjango = { 'djlint' },
+        django = { 'djlint' },
         markdown = { 'mdformat' },
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -210,7 +210,28 @@ return {
       local servers = {
         clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                autoImportCompletions = true,
+                typeCheckingMode = 'basic',
+              },
+              venvPath = '.',
+              venv = 'venv',
+            },
+          },
+        },
+        djangohtml = {
+          filetypes = { 'htmldjango', 'html' },
+        },
+        ruff_lsp = {
+          init_options = {
+            settings = {
+              -- Ruff settings if needed
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -218,7 +239,7 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
 
         lua_ls = {
